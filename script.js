9553a8c72860dfd5ce1499715dc9b6caf7a2066c
eido <48271387+eido8@users.noreply.github.com>
2019-08-31 11:03:40 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+    setTimeout(
+        function() {
+            document.querySelector(".logo").style.opacity = "1";
+        }, 2100
+    );
+    
+    document.querySelector(".myVideo").style.opacity = "1";
+    document.querySelector(".h1-1").style.opacity = "1";
+    
+    setTimeout(
+        function() {
+            document.querySelector(".h1-2").style.opacity = "1";
+        }, 700
+    );
+    setTimeout(
+        function() {
+            document.querySelector(".h1-3").style.opacity = "1";
+        }, 1400
+    );
+}
+
+
+// Navbar Slide in/out
+var clicked = false;
+
+function bringNav() {
+    if (clicked == false) {
+        document.querySelector(".burger").className = "burger-on";
+        document.querySelector(".nav-links").className = "nav-links-on";
+        clicked = true;
+    } else {
+        document.querySelector(".burger-on").className = "burger";
+        document.querySelector(".nav-links-on").className = "nav-links";
+        clicked = false;
+    }
+}
+
+
